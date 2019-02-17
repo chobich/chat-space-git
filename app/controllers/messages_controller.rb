5cf4a9f7d6098a4bfd96debbe128e215e521b798
chobich <cat.chobi0608@gmail.com>
2019-02-17 21:02:30 +0900

    Revert "Merge branch 'ajax'"
    This reverts commit 3cdc717ce286fc681f1e44104c809179b1b8f0f2, reversing
    changes made to a1432884515032489574a92816c20f704a6bac6e.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,14 +9,18 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-          respond_to do |format|
-      format.html
-      format.json
-    end
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-      render json: nil
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
+
+    def show_group_menber
+    puts "hellow world"
+  end
+
 
   private
 
@@ -27,5 +31,4 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
-
 end
--- app/views/messages/create.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.content @message.content
-json.user_name @message.user.name
-json.group_id @message.group.id
-json.image @message.image.url
-json.time @message.created_at
