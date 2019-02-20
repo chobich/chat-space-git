cf2e1992e2e5776b31f0df2c516713bf51888458
chobich <cat.chobi0608@gmail.com>
2019-02-20 20:58:19 +0900

    [WIP][chat-space]インクリメンタルサーチの実装
    #WHAT
    グループメンバーのインクリメンタルサーチの実装
    
    #WHY
    グループ編集画面でメンバの追加・削除を非同期通信で行うため。
    
    Gyazo
    https://gyazo.com/42cd8373db678d5319d69b34348c2b62

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20).where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -16,4 +25,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
