1087c509b7fe6d41554b949d3195330d28839113
chobich <cat.chobi0608@gmail.com>
2019-03-04 07:37:15 +0900

    viewの修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,32 @@
 $(document).on('turbolinks:load', function() {
   function buildSendMessageHTML(message){
-    var messageImage = message.image ? message.image:""
-    var html =`<div class="right" data-id="${message.id}">
-                 <div class="upper-message">
-                  <div class="upper-message__chat_name">
-                   ${message.user_name}
-                  </div>
-                  <div class="upper-message__chat_time">
-                   ${message.created_at}
-                  </div>
-                 </div>
-               <div class="lower-message">
-                 <div class="lower-message__chat_comment">
-                  ${message.content}
-                  <img src="${messageImage}">
-                 </div>
-               </div>
-              </div>`
+    messageImage = message.image ? message.image:''
+    var html =
+    `
+    <div class="message" data-id="${message.id}">
+      <div class="upper-message">
+        <div class="upper-message__chat_name">
+        ${message.user_name}
+        </div>
+        <div class="upper-message__chat_time">
+        ${message.created_at}
+        </div>
+      </div>
+      <div class="lower-message">
+      <div class="lower-message__chat_comment">
+       ${message.content}
+       <img src="${messageImage}">
+       </div>
+       </div>
+       </div>
+       `
     return html;
 }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href
+    var href = $(this).attr('action');
     $.ajax({
       url: href,
       type: "POST",
@@ -30,8 +34,8 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json',
       processData: false,
       contentType: false
-    }
-    )
+    })
+
     .done(function(message_data,status, xhr){
       var html = buildSendMessageHTML(message_data);
       $('.message-box').append(html).animate({scrollTop: $('.message-box')[0].scrollHeight});
@@ -46,10 +50,11 @@ $(document).on('turbolinks:load', function() {
 
 $(function(){
     setInterval(updateMessage, 5000);
+  });
+
   function updateMessage(){
     if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-
-    var latest_message_id = $('.right').last().data('id');
+    var latest_message_id = $('.message').last().data('id');
     $.ajax({
       url: location.href,
       type: 'GET',
@@ -62,8 +67,7 @@ $(function(){
         new_message.forEach(function(message){
         insertHTML = buildSendMessageHTML(message);
       });
-      $('.message-box').append(insertHTML);
-      $('.message-box').animate({scrollTop: $('.message-box')[0].scrollHeight});
+      $('.message-box').append(insertHTML).animate({scrollTop: $('.message-box')[0].scrollHeight});
     })
     .fail(function(){
       alert('自動更新に失敗しました');
@@ -72,6 +76,4 @@ $(function(){
       clearInterval(updateMessage);
     }
   }
-});
-
 });
